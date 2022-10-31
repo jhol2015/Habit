c3fd7f8951fd4046fd4f72696796c986929d7d73
Jhol Moreira Mendanha <galilleu2011@gmail.com>
2022-10-30 21:47:05 -0300

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Jhol Moreira on 30/10/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
