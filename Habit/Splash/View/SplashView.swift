1d553d031f6bfec6b893e98027e2cfd2d9d36532
Jhol Moreira Mendanha <galilleu2011@gmail.com>
2022-10-30 21:54:20 -0300

    First Commit project Habit

---
+++ Habit/Splash/View/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  Habit
+//
+//  Created by Jhol Moreira on 30/10/22.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
