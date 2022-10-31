bd2b8cb422f60ca06d3ac37d3f57c06250bab666
Jhol Moreira Mendanha <galilleu2011@gmail.com>
2022-10-30 23:19:38 -0300

    Create alert and insert logo in SplashScreen

---
+++ Habit/Splash/Model/SplashUIState.swift
@@ -0,0 +1,15 @@
+//
+//  SplashUIState.swift
+//  Habit
+//
+//  Created by Jhol Moreira on 30/10/22.
+//
+
+import Foundation
+
+enum SplashUIState {
+    case loading
+    case goToSignInScreen
+    case goToHomeScreen
+    case error(String)
+}
--- Habit/Splash/View/SplashView.swift
+++ Habit/Splash/View/SplashView.swift
@@ -8,13 +8,78 @@
 import SwiftUI
 
 struct SplashView: View {
+    @State var state: SplashUIState = .goToSignInScreen
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        switch state {
+        case .loading:
+            loadingView()
+        case .goToSignInScreen:
+            Text("Carregar tela de login")
+        case .goToHomeScreen:
+            Text("Carregar tela principal")
+        case .error(let msg):
+            loadingView(error: msg)
+        }
+    }
+}
+
+//Forma 1 - Compartilhamento | Objetos = (passa um objeto LoadingView())
+struct LoadingView: View {
+    var body: some View {
+        ZStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(20)
+                .background(Color.white)
+                .ignoresSafeArea()
+        }
+    }
+}
+
+//Forma 2 - Variaveis de extensions (ao mandar um objeto manda uma variavel = loading)
+extension SplashView {
+    var loading: some View {
+        ZStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(20)
+                .background(Color.white)
+                .ignoresSafeArea()
+        }
+    }
+}
+
+// Forma 3 - Funções em extensions (manda o objeto loadingView())
+extension SplashView {
+    func loadingView(error: String? = nil) -> some View {
+        ZStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(20)
+                .background(Color.white)
+                .ignoresSafeArea()
+            
+            if let error = error {
+                Text("")
+                    .alert(isPresented: .constant(true)){
+                        Alert(title: Text("Habit"), message: Text(error), dismissButton: .default(Text("Ok")){
+                            // faz algo quando some o alerta
+                        })
+                    }
+            }
+        }
     }
 }
 
 struct SplashView_Previews: PreviewProvider {
     static var previews: some View {
-        SplashView()
+        SplashView(state: .loading)
     }
 }
